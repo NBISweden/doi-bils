8b1df2e67147bc6c2a103f07e3200c3459700144
jonandernovella <novella.uu@gmail.com>
2019-03-27 13:14:27 +0100

    Updated package dependencies with new libs

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ except ImportError:
 
 config = {
     'description': 'NBIS works website and API',
-    'version': '1.7',
+    'version': '3.0',
     'install_requires': [
         'flask==1.0.2',
         'flask-restplus==0.12.1',
-        'requests==2.21.0'
+        'requests==2.21.0',
+        'gevent==1.4.0',
+        'cryptography==2.6.1'
     ],
     'scripts': [],
     'name': 'doi-bils'
