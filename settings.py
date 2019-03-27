38273df3773786bced8c4910285164b4a699faa8
jonandernovella <novella.uu@gmail.com>
2019-03-27 13:13:50 +0100

    Added ssl settings for gevent

--- settings.py
+++ settings.py
@@ -8,3 +8,7 @@ RESTPLUS_SWAGGER_UI_DOC_EXPANSION = 'list'
 RESTPLUS_VALIDATE = True
 RESTPLUS_MASK_SWAGGER = False
 RESTPLUS_ERROR_404_HELP = False
+
+# Gevent settings
+CERT_FILE = 'certs/ssl.cert'
+KEY_FILE = 'certs/ssl.key'
