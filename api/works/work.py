321877dc9d447a18d538060b24676ac07276cb5c
jonandernovella <novella.uu@gmail.com>
2019-09-18 12:31:26 +0200

    Refactor work by id method
    The work by id method now points at the new data cite api and processes a single doi at a time

--- api/works/work.py
+++ api/works/work.py
@@ -4,40 +4,41 @@ import logging
 log = logging.getLogger(__name__)
 
 def get_all_works():
-    url = 'https://api.datacite.org/works'
+    url = 'https://api.datacite.org/dois'
     headers = {'Content-Type': 'application/json'}
-    params = '?data-center-id=snd.bils'
+    params = '?client-id=snd.bils'
     response = requests.get(url + params, headers=headers)
     assert response.status_code == 200
     works_array = []
 
     for item in response.json()['data']:
         work_dict = dict()
-        work_dict['title']=item['attributes'].get('title', '')
-        work_dict['description']=item['attributes'].get('description', '')
+        work_dict['title']=item['attributes']['titles'][0].get('title', '')
+        work_dict['description']=item['attributes']['descriptions'][0].get('description', '')
         work_dict['year']=item['attributes'].get('published', '')
         work_dict['doi']=item['attributes'].get('doi', '')
-        work_dict['access_constraints']=item['attributes'].get('license', '')
-        work_dict['authors']=item['attributes'].get('author', [])
+        work_dict['access_constraints']=item['attributes'].get('rightsList', '')
+        work_dict['authors']=item['attributes'].get('creators', [])
+
         works_array.append(work_dict)
 
     return works_array
 
-def get_work_by_id(doi):
-    url = 'https://api.datacite.org/works'
+def get_work_by_id(identifier):
+    url = 'https://api.datacite.org/dois/'
     headers = {'Content-Type': 'application/json'}
-    params = r'?query=doi:{}&data-center-id=snd.bils'.format(doi)
+    params = '{}'.format(identifier)
     log.info(params)
     response = requests.get(url + params, headers=headers)
     assert response.status_code == 200
+    item = response.json()['data']
     work_dict = dict()
 
-    for item in response.json()['data']:
-        work_dict['title']=item['attributes'].get('title', '')
-        work_dict['description']=item['attributes'].get('description', '')
-        work_dict['year']=item['attributes'].get('published', '')
-        work_dict['doi']=item['attributes'].get('doi', '')
-        work_dict['access_constraints']=item['attributes'].get('license', '')
-        work_dict['authors']=item['attributes'].get('author', [])
+    work_dict['title']=item['attributes']['titles'][0].get('title', '')
+    work_dict['description']=item['attributes']['descriptions'][0].get('description', '')
+    work_dict['year']=item['attributes'].get('published', '')
+    work_dict['doi']=item['attributes'].get('doi', '')
+    work_dict['access_constraints']=item['attributes'].get('rightsList', '')
+    work_dict['authors']=item['attributes'].get('creators', [])
 
     return work_dict
